03a459c3758bd082bff683e45702ee2c7858dd7a
Christian Hotz-Behofsits <christian.hotz-behofsits@tuwien.ac.at>
2013-05-24 17:05:24 +0200

    Models added

---
+++ src/models/Message.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Message extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'messages';
+
+	public function conversation(){
+		return $this->belongsTo('Conversation');
+	}
+
+	public function user(){
+		return $this->belongsTo('User');
+	}
+
+}
