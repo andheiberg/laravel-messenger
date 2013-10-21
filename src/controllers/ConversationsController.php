0e44741c109430c0c7e451c09378941f49a7f561
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2013-10-21 12:39:57 +0200

    bugix

--- src/controllers/ConversationsController.php
+++ src/controllers/ConversationsController.php
@@ -1,13 +1,11 @@
 <?php
 namespace Pichkrement\Messenger\Controllers;
 
-use Pichkrement\Messenger\Models;
-
 class ConversationsController extends \BaseController{
 
     public function info(){
 
-    	return View::make('info', array('msgnum' => Messsage::count()));
+    	return \View::make('info', array('msgnum' => \Pichkrement\Messenger\Models\Message::count()));
     }
 
 }
