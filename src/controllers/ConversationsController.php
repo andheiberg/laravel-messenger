0645d99360abaa803a1faef1565a61b732e5a357
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2013-10-26 10:50:35 +0200

    refactored testcases and controller

--- src/controllers/ConversationsController.php
+++ src/controllers/ConversationsController.php
@@ -1,7 +1,6 @@
 <?php
 namespace Pichkrement\Messenger\Controllers;
 
-use \Pichkrement\Messenger\Lib\Helper as H;
 use \Pichkrement\Messenger\Models\User as User;
 use \Pichkrement\Messenger\Models\Message as Message;
 use \Pichkrement\Messenger\Models\Conversation as Conversation;
@@ -14,26 +13,10 @@ class ConversationsController extends \BaseController{
     	$numusr = User::count();
     	$numcon = Conversation::count();
 
-
-    	// test
-
-    	// remove all Users
-    	H::removeAll(User::all());
-
-    	H::removeAll(Conversation::all());
-
-
-    	$sender = H::createRandomUser();
-    	$rec = H::createRandomUser();
-
-    	$sender->send($rec, "testnachricht");
-
-
     	$return = "<html><head></head><body>
     		Messages: $nummsg <br/>
     		Users: $numusr <br/>
     		Konversations: $numcon <br/>";
-
 
     		//conversations
     		$return .= "<table>
@@ -57,7 +40,7 @@ class ConversationsController extends \BaseController{
     		$return .= "
     	</body></html>";
 
-    	return $return; //\View::make('info', array('msgnum' => \Pichkrement\Messenger\Models\Message::count()));
+    	return $return;
     }
 
 }
--- tests/MyFirstTest.php
+++ tests/MyFirstTest.php
@@ -1,6 +1,28 @@
 <?php
 
-class MyFirstTest extends PHPUnit_Framework_TestCase {
+use \Pichkrement\Messenger\Lib\Helper as H;
+use \Pichkrement\Messenger\Models\User as User;
+use \Pichkrement\Messenger\Models\Message as Message;
+use \Pichkrement\Messenger\Models\Conversation as Conversation;
+
+class MyFirstTest extends TestCase {
+
+
+	public function setUp(){
+
+		parent::setUp();
+
+		H::removeAll(User::all());
+
+    	H::removeAll(Conversation::all());
+	}
+
+	public function testSendMail(){
+		$sender = H::createRandomUser();
+    	$rec = H::createRandomUser();
+
+    	$sender->send($rec, "testnachricht");
+	}
 
 	public function testMe(){
 		$this->assertFalse(false);
