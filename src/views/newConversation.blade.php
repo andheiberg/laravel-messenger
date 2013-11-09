59e9d6f6a2168702b6ebb263f3e568277e01b2dd
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2013-11-09 15:13:52 +0100

    bugfix

--- src/views/newConversation.blade.php
+++ src/views/newConversation.blade.php
@@ -1,12 +1,36 @@
-{{ Form::open()}}
-
 <h3>New Conversation</h3>
-{{Form::label('convName', 'Conversation Name')}}
-{{Form::text('convName')}}
-{{Form::label('participants', 'Participants')}}
-{{Form::textarea('participants')}}
-{{Form::label('message', 'Message')}}
-{{Form::textarea('message')}}
-{{Form::submit('Send')}}
+
+{{ Form::open(array('class'=>'form-horizontal'))}}
+
+
+  <div class="form-group">
+	{{Form::label('convName', 'Conversation Name', array('class' => 'col-sm-2'))}}
+
+    <div class="col-sm-10">
+    	{{Form::text('convName', null,  array('class' => 'form-control'))}}
+    </div>
+  </div>
+
+  <div class="form-group">
+	{{Form::label('participants', 'Participants', array('class' => 'col-sm-2'))}}
+
+    <div class="col-sm-10">
+    	{{Form::text('participants', null, array('class' => 'form-control'))}}
+    </div>
+  </div>
+
+  <div class="form-group">
+  	{{Form::label('message', 'Message', array('class' => 'col-sm-2'))}}
+
+    <div class="col-sm-10">
+    	{{Form::textarea('message', null, array('class' => 'form-control'))}}
+    </div>
+  </div>
+
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+    	{{Form::submit('Send', array('class' => 'btn btn-default'))}}
+    </div>
+  </div>
 
 {{Form::close()}}
--- src/views/newMessage.blade.php
+++ src/views/newMessage.blade.php
@@ -1,10 +1,24 @@
-{{ Form::open()}}
-
 <h3>New Conversation</h3>
-{{Form::label('participants', 'Add Participants')}}
-{{Form::textarea('participants')}}
-{{Form::label('message', 'Message')}}
-{{Form::textarea('message')}}
-{{Form::submit('Send')}}
+
+{{ Form::open(array('class'=>'form-horizontal'))}}
+
+
+  <div class="form-group">
+    {{Form::label('participants', 'Add Participants')}}
+    <div class="col-sm-10">
+    	{{Form::textarea('participants', array('class' => 'form-control'))}}
+    </div>
+  </div>
+  <div class="form-group">
+  	{{Form::label('message', 'Message', array('class' => 'col-sm-2 control-label'))}}
+    <div class="col-sm-10">
+    	{{Form::textarea('message', array('class' => 'col-sm-2 control-label'))}}
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+    	{{Form::submit('Send', array('class' => 'btn btn-default'))}}
+    </div>
+  </div>
 
 {{Form::close()}}
